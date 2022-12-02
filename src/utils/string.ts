a9d81c8b643cc666e5d052e00e0cc222aa477b5d
Ngô Văn Thiện <39598876+thiengo105@users.noreply.github.com>
2022-12-03 01:37:18 +0700

    update with replaceY

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -4,7 +4,8 @@ export function normalizeVietnamese(vietnameseText: string) {
   const replaceD = replaceA.replace(/[dđ]/g, "d");
   const replaceE = replaceD.replace(/[éèẻẽẹêếềểễệ]/g, "e");
   const replaceI = replaceE.replace(/[íìỉĩị]/g, "i");
-  const replaceO = replaceI.replace(/[óòỏõọôốồổỗộơớờởỡợ]/g, "o");
+  const replaceY = replaceI.replace(/[ýỳỷỹỵ]/g, "i");
+  const replaceO = replaceY.replace(/[óòỏõọôốồổỗộơớờởỡợ]/g, "o");
   const replaceU = replaceO.replace(/[úùủũụưứừửữự]/g, "u");
   return replaceU;
 }
